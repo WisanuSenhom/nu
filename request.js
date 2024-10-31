2ecaa1a148269e0b103a2cdef314c2af5b123e41
Wisanu Senhom <48409780+WisanuSenhom@users.noreply.github.com>
2024-10-31 09:33:40 +0700

    Update request.js

--- request.js
+++ request.js
@@ -10,6 +10,7 @@ Swal.fire({
     showCancelButton: true,
     confirmButtonText: 'ตกลง',
     cancelButtonText: 'ดำเนินการ',
+    allowOutsideClick: false,
     customClass: {
         confirmButton: "btn btn-success",
         cancelButton: "btn btn-danger"
