2984841d15edf456af988f16bd1066a77071e538
Wisanu Senhom <48409780+WisanuSenhom@users.noreply.github.com>
2024-12-02 16:11:48 +0700

    Update login.js

--- login.js
+++ login.js
@@ -1,105 +1,36 @@
-async function getProfile() {
-  const profile = await liff.getProfile();
-  const displayName = profile.displayName + " LINE";
-  const yourid = profile.userId;
-  const yourpic = profile.pictureUrl;
-  getmember(yourid, yourpic, displayName);
-}
+document.addEventListener("DOMContentLoaded", function () {
+  const uuid = localStorage.getItem("uuid");
+  if (uuid) {
+    window.location.href = "index.html"; 
+  }
+});
 
-async function getmember(yourid, yourpic, profile) {
-  // localStorage.clear();
-  // console.log(profile);
-  showLoading();
-  let gas = `https://script.google.com/macros/s/AKfycbyY-5A1mpNjJjD9CjPEX4fSW5N6xB7PoMAODHgjMJuuLARrCjvm5csgFamB8MKbjUB9/exec?id=${yourid}&profile=${profile}`;
-  const records = await fetch(gas);
-  const data = await records.json();
-  //  // console.log(data.user);
-  if (data.user === null || data.user === undefined || data.user == 0) {
-    Swal.fire({
-      icon: "error",
-      title: "ไม่พบข้อมูลของคุณในระบบ หรือไม่พบการเชื่อมต่อ Telegram",
-      text: "หากคุณ Login ด้วย Telegram โปรด Login ด้วย Line ก่อน แล้วทำการ เชื่อมต่อ Telegram ที่เมนูหน้าลงเวลา",
-      allowOutsideClick: false,
-      confirmButtonText: "ลองอีกครั้ง", // กำหนดชื่อปุ่มยืนยัน
-      showCancelButton: true, // แสดงปุ่มยกเลิก
-      cancelButtonText: "ยกเลิก", // กำหนดชื่อปุ่มยกเลิก
-      showDenyButton: true, // แสดงปุ่ม Deny
-      denyButtonText: "สมัครสมาชิกใหม่", // กำหนดชื่อปุ่ม Deny
-      confirmButtonColor: "#008000",
-      cancelButtonColor: '#0073E6'
-    }).then((result) => {
-      // ตรวจสอบผลการตอบสนองจากผู้ใช้
-      if (result.isConfirmed) {
-        // หากกดปุ่มยืนยัน
-        getProfile();
-      } else if (result.isDenied) {
-        // หากกดปุ่ม Deny
-        // console.log("Deny");
-        window.location.href = "register.html"; // ไปยัง register.html
-      }
-      try {
-        liff.closeWindow();
-      } catch (error) {
-        // console.error("Failed to close window, refreshing...");
-        location.reload(); // รีเฟรชหน้า
-      }
+function clearLocal() {
+  // แสดงข้อความยืนยันก่อนลบข้อมูลใน Local Storage
+  Swal.fire({
+    title: "คุณแน่ใจหรือไม่?",
+    text: "การดำเนินการนี้จะลบข้อมูลทั้งหมดใน Local Storage อย่างถาวร",
+    icon: "warning",
+    showCancelButton: true, // แสดงปุ่มยกเลิก
+    confirmButtonColor: "#d33", // สีปุ่มยืนยัน
+    cancelButtonColor: "#3085d6", // สีปุ่มยกเลิก
+    confirmButtonText: "ใช่, ลบข้อมูล!", // ข้อความบนปุ่มยืนยัน
+    cancelButtonText: "ยกเลิก", // ข้อความบนปุ่มยกเลิก
+  }).then((result) => {
+    if (result.isConfirmed) {
+      // หากผู้ใช้กดปุ่มยืนยัน ให้ลบข้อมูลใน Local Storage
+      localStorage.clear();
 
-      // Use a timeout to refresh the page after trying to close the window
-      setTimeout(() => {
-        location.reload(); // Refresh if liff.closeWindow() does not work
-      }, 500); // Adjust the delay time as needed (500ms in this case)
-    });
-  } else {
-    localStorage.setItem("yourpic", yourpic);
-    data.user.forEach(function (user) {
-      localStorage.setItem("uuid", user.uuid);
-      localStorage.setItem("cidhash", user.cidhash);
-      localStorage.setItem("userid", user.userid);
-      localStorage.setItem("name", user.name);
-      localStorage.setItem("job", user.job);
-      localStorage.setItem("mainsub", user.mainsub);
-      localStorage.setItem("office", user.office);
-      localStorage.setItem("oflat", user.oflat);
-      localStorage.setItem("oflong", user.oflong);
-      localStorage.setItem("db1", user.db1);
-      localStorage.setItem("token", user.token);
-      localStorage.setItem("status", user.status);
-      localStorage.setItem("role", user.role);
-      localStorage.setItem("boss", user.boss);
-      localStorage.setItem("ceo", user.ceo);
-      localStorage.setItem("docno", user.docno);
-      localStorage.setItem("upic", user.upic);
-      localStorage.setItem("refid", user.refid);
-      localStorage.setItem("rank", user.rank);
-
+      // แสดงข้อความแจ้งเตือนเมื่อการลบเสร็จสิ้น
       Swal.fire({
         confirmButtonColor: "#0ef",
         icon: "success",
-        title: "ลงชื่อเข้าใช้สำเร็จแล้ว",
-        allowOutsideClick: false,
-      }).then((result) => {
-        // ตรวจสอบว่าผู้ใช้กดปุ่มตกลงหรือไม่
-        if (result.isConfirmed) {
-          // กระทำที่ต้องการทำหลังจากกดปุ่มตกลง
-          window.location.href = "index.html";
-          // console.log("สำเร็จ");
-        }
+        title: "ข้อมูลใน Local Storage ถูกลบเรียบร้อยแล้ว",
       });
-    });
-    hideLoading();
-  }
-}
-
-function clearLocal() {
-  // เรียกใช้ localStorage.clear() เพื่อลบข้อมูลทั้งหมดใน Local Storage
-  localStorage.clear();
-
-  Swal.fire({
-    confirmButtonColor: "#0ef",
-    icon: "success",
-    title: "Local Storage has been cleared.",
+    }
   });
 }
+
 
 function showLoading() {
   var overlay = document.getElementById("loadingOverlay");
@@ -111,27 +42,75 @@ function hideLoading() {
   overlay.style.display = "none";
 }
 
+
+async function getProfile() {
+  try {
+    const profile = await liff.getProfile();
+    const displayName = profile.displayName;
+    const yourid = profile.userId;
+    const yourpic = profile.pictureUrl;
+    const yourstatus = profile.statusMessage;
+
+    Swal.fire({
+      title: `ยินดีต้อนรับคุณ\n${displayName}`,
+      html: `สถานะ : ${yourstatus}<br><br><strong>คุณต้องเข้าสู่ระบบด้วยบัญชีไลน์นี้หรือไม่?</strong>`,
+      imageUrl: yourpic,
+      imageWidth: 150,
+      imageHeight: 150,
+      imageAlt: "User Photo",
+      showCancelButton: true,
+      confirmButtonText: "ยืนยัน",
+      cancelButtonText: "ยกเลิก",
+      confirmButtonColor: "#00B900",
+      cancelButtonColor: "#d33",
+      allowOutsideClick: false,
+    }).then((result) => {
+      if (result.isConfirmed) {
+        getmember(yourid, yourpic, displayName, "line");
+      } else {
+        Swal.fire({
+          icon: "info",
+          title: "คุณเลือกยกเลิกการเข้าสู่ระบบ",
+          confirmButtonColor: "#00B900",
+        });
+      }
+    });
+  } catch (error) {
+    console.error("Error retrieving profile:", error);
+    Swal.fire({
+      icon: "error",
+      title: "เกิดข้อผิดพลาด",
+      text: "ไม่สามารถดึงข้อมูลโปรไฟล์ได้",
+      confirmButtonColor: "#d33",
+    });
+  }
+}
+
+
 // Line Login
 async function main() {
   hideLoading();
-   await liff.init({ liffId: "1654797991-pr0xKPxW" });
-//  await liff.init({ liffId: "1654797991-Xmxp3Gpj" });
+  await liff.init({ liffId: "1654797991-pr0xKPxW" }); 
+ //  await liff.init({ liffId: "1654797991-Xmxp3Gpj" }); // ทดสอบ
   if (liff.isLoggedIn()) {
     getProfile();
   } else {
     liff.login();
   }
 }
-// main();
 
-// Telegram Login
 
+// Telegram Login 
 const botUsername = "TimestampNotifybot"; // Bot username
 const botId = "7733040493"; // Bot ID from @BotFather
 
+// Telegram Login Test
+// const botUsername = "wisanusenhombot"; // Bot username
+// const botId = "7781010431"; // Bot ID from @BotFather 
+
 function telegramLogin() {
-  // const authUrl = `https://oauth.telegram.org/auth?bot_id=${botId}&origin=${window.location.origin}&embed=1`;
-  const authUrl = `https://oauth.telegram.org/auth?bot_id=${botId}&origin=https://wisanusenhom.github.io/nu/login.html&embed=1`;
+  const originUrl = "https://wisanusenhom.github.io/nu/login.html"
+  const authUrl = `https://oauth.telegram.org/auth?bot_id=${botId}&origin=${originUrl}&embed=1`;
   window.open(authUrl, "_self"); // Open Telegram login page
 }
 
@@ -148,20 +127,19 @@ async function handleTelegramCallback() {
     try {
       // ถอดรหัส Base64
       const telegramData = atob(telegramDataBase64);
-      // console.log("Decoded Telegram Data:", telegramData); // ตรวจสอบข้อมูลที่ถอดรหัส
-
+     // console.log("Decoded Telegram Data:", telegramData); // ตรวจสอบข้อมูลที่ถอดรหัส
       // แปลงข้อมูลเป็น JSON
       const user = JSON.parse(telegramData);
-      const { id, first_name, last_name, photo_url } = user;
+      const { id, first_name, last_name, photo_url,username } = user;
 
       // console.log("Telegram User Data:", user);
 
       const result = await Swal.fire({
-        title: `ยินดีต้อนรับ \n , ${first_name} ${last_name}`,
-        text: `คุณต้องการดำเนินการต่อด้วยบัญชีนี้หรือไม่?`,
+        title: `ยินดีต้อนรับคุณ\n${first_name} ${last_name}`,
+        html: `คุณต้องการเข้าสู่ระบบด้วยเทเลแกรม<br>ไอดี : ${id} <br> บัญชี : ${username} หรือไม่? `,
         imageUrl: photo_url,
-        imageWidth: 100,
-        imageHeight: 100,
+        imageWidth: 150,
+        imageHeight: 150,
         imageAlt: "User Photo",
         showCancelButton: true,
         confirmButtonText: "ยืนยัน",
@@ -174,7 +152,7 @@ async function handleTelegramCallback() {
       if (result.isConfirmed) {
         // หากผู้ใช้กดยืนยัน ให้ดำเนินการกับข้อมูล user
         localStorage.setItem("chatId", id);
-        await getmember(id, photo_url, `${first_name} ${last_name}`);
+        await getmember(id, photo_url,username,"telegram");        
       } else {
         Swal.fire({
           icon: "info",
@@ -184,7 +162,7 @@ async function handleTelegramCallback() {
       }
       window.location.hash = "";
     } catch (error) {
-    //  showNoMessageAlert();
+      //  showNoMessageAlert();
       Swal.fire({
         icon: "error",
         title: "Error parsing Telegram data.",
@@ -200,29 +178,93 @@ async function handleTelegramCallback() {
     // });
   }
 }
+async function getmember(yourid, yourpic, profile, useapp) {
+   showLoading();
 
-function showNoMessageAlert() {
-  const telegramqr =
-    "https://lh5.googleusercontent.com/d/1aC5SsCMqeGgYIBzwNRdXnrjTZCyANIg-";
-  Swal.fire({
-    title: "ไม่พบ message.chat.id",
-    html: `
-      <p>กรุณาตรวจสอบว่าคุณได้เพิ่มเพื่อน "<strong>ลงเวลา</strong>" แล้วหรือยัง</p>
-      <p>หากยังไม่ได้เพิ่มเพื่อน กรุณาเพิ่มเพื่อนก่อนโดยคลิกที่ปุ่มด้านล่าง:</p>
-    `,
-    imageUrl: telegramqr,
-    imageWidth: 200,
-    imageHeight: 240,
-    imageAlt: "QR Code",
-    icon: "error",
-    confirmButtonText: "เพิ่มเพื่อน",
-    cancelButtonText: "ยกเลิก",
-    confirmButtonColor: "#24A1DE",
-    cancelButtonColor: "#6F7378",
-    showCancelButton: true,
-  }).then((result) => {
-    if (result.isConfirmed) {
-      window.open("https://t.me/TimestampNotifyBot", "_blank");
+  // ฟังก์ชันแสดง Swal
+  function displaySwal(icon, title, text, confirmButtonText, cancelButtonText, denyButtonText) {
+    return Swal.fire({
+      icon: icon,
+      title: title,
+      text: text,
+      allowOutsideClick: false,
+      confirmButtonText: confirmButtonText,
+      cancelButtonText: cancelButtonText,
+      denyButtonText: denyButtonText,
+      showCancelButton: !!cancelButtonText,
+      showDenyButton: !!denyButtonText,
+      confirmButtonColor: "#008000",
+      cancelButtonColor: "#0073E6",
+    });
+  }
+
+  // ฟังก์ชันบันทึกข้อมูลผู้ใช้ลงใน Local Storage
+  function saveUserToLocalStorage(user) {
+    for (let key in user) {
+      localStorage.setItem(key, user[key]);
     }
-  });
+  }
+
+  try {
+    let gas = `https://script.google.com/macros/s/AKfycbyY-5A1mpNjJjD9CjPEX4fSW5N6xB7PoMAODHgjMJuuLARrCjvm5csgFamB8MKbjUB9/exec?id=${yourid}&profile=${profile}`;
+    const records = await fetch(gas);
+    const data = await records.json();
+
+    if (!data.user) {
+      // กรณีไม่พบข้อมูลผู้ใช้
+      localStorage.removeItem("chatId");
+      let headertext, bodytext;
+      if (useapp === "line") {
+        headertext = "ไม่พบข้อมูลของคุณในระบบ";
+        bodytext = "กรุณาสมัครสมาชิกก่อนใช้งาน";
+      } else if (useapp === "telegram") {
+        headertext = "ไม่พบการเชื่อมต่อ Telegram";
+        bodytext = "โปรด Login ด้วย Line ก่อน แล้วทำการ เชื่อมต่อ Telegram ที่เมนูหน้าลงเวลา";
+      }
+
+      const result = await displaySwal(
+        "error",
+        headertext,
+        bodytext,
+        "ลองอีกครั้ง",
+        "ยกเลิก",
+        "สมัครสมาชิกใหม่"
+      );
+
+      if (result.isConfirmed) {
+        main();
+      }
+      if (result.isDenied) {
+        window.location.href = "register.html"; // ไปยังหน้าสมัครสมาชิก
+      }
+
+      try {
+        liff.closeWindow();
+      } catch (error) {
+        setTimeout(() => location.reload(), 500); // รีเฟรชหน้าในกรณีปิดหน้าต่างไม่สำเร็จ
+      }
+    } else {
+      // กรณีพบข้อมูลผู้ใช้
+      localStorage.setItem("yourpic", yourpic);
+      data.user.forEach(saveUserToLocalStorage);
+
+      Swal.fire({
+        icon: "success",
+        title: "ลงชื่อเข้าใช้สำเร็จแล้ว",
+        confirmButtonColor: "#0ef",
+        allowOutsideClick: false,
+      }).then(() => {
+        window.location.href = "index.html"; // ไปยังหน้าแรก
+      });
+    }
+  } catch (error) {
+    // กรณีเกิดข้อผิดพลาดระหว่างการเรียก API
+    Swal.fire({
+      icon: "error",
+      title: "ข้อผิดพลาด",
+      text: "ไม่สามารถดึงข้อมูลได้ กรุณาลองใหม่อีกครั้ง",
+    });
+  } finally {
+    hideLoading(); // ซ่อนหน้าจอโหลด
+  }
 }
