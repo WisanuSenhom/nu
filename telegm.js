5554f716fc0dd9b3e513f0fc7cd70c19b643c94b
Wisanu Senhom <48409780+WisanuSenhom@users.noreply.github.com>
2025-02-11 11:40:33 +0700

    Update telegm.js

--- telegm.js
+++ telegm.js
@@ -264,7 +264,7 @@ async function handleTelegramCallback(){
         currentProgressStep: 1,
       })
       await Queue.fire({
-        html: `หาก Telegram ไม่แจ้งเตือน เปิดแอพ Telegram <br>
+        html: `หาก Telegram ไม่แจ้งเตือน เปิดแอพ/แชท Telegram <br>
         <img src="https://lh5.googleusercontent.com/d/1MA-oSuWWLdixZXEK2ChzfEBxitWRJ6O0" alt="ตัวอย่างการกดยืนยันใน Telegram" 
         style="width: 100%; max-width: 300px; border-radius: 10px; box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);">`,
         currentProgressStep: 2,
